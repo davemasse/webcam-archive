4324612fb7a8bbf771f71ada74c598ba68a06a0e
Dave Masse <dmasse@rudemoose.com>
2011-01-12 16:43:49 -0500

    Don't set explicit width and height on images for the time being.

--- display_frontend.php
+++ display_frontend.php
@@ -6,14 +6,14 @@
 					if ($key == 0) {
 						?>
 							<div class="thumb">
-								<a href="<?php echo $upload_path . '/' . self::upload_dir . '/' . date('Y/m/d/', $entry_date) . $entry_date . '/' . $entry['sizes'][2]['id']; ?>.jpg" title="<?php echo date('n/j/Y \a\t g:i a', $entry_date); ?><br /><?php foreach ($entry['metas'] as $meta) : ?><?php echo $meta['name']; ?>: <?php echo $meta['value']; ?><br /><?php endforeach; ?>"><img src="<?php echo $upload_path . '/' . self::upload_dir . '/' . date('Y/m/d/', $entry_date) . $entry_date . '/' . $size['id']; ?>.jpg" width="<?php echo $size['width']; ?>" height="<?php echo $size['height']; ?>" /></a><br />
+								<a href="<?php echo $upload_path . '/' . self::upload_dir . '/' . date('Y/m/d/', $entry_date) . $entry_date . '/' . $entry['sizes'][2]['id']; ?>.jpg" title="<?php echo date('n/j/Y \a\t g:i a', $entry_date); ?><br /><?php foreach ($entry['metas'] as $meta) : ?><?php echo $meta['name']; ?>: <?php echo $meta['value']; ?><br /><?php endforeach; ?>"><img src="<?php echo $upload_path . '/' . self::upload_dir . '/' . date('Y/m/d/', $entry_date) . $entry_date . '/' . $size['id']; ?>.jpg" /></a><br />
 								<?php echo date('g:i a', $entry_date); ?>
 							</div>
 						<?php
 					} elseif ($key == 1) {
 						?>
 							<div class="tooltip">
-								<img src="<?php echo $upload_path . '/' . self::upload_dir . '/' . date('Y/m/d/', $entry_date) . $entry_date; ?>/<?php echo $size['id']; ?>.jpg" width="<?php echo $size['width']; ?>" height="<?php echo $size['height']; ?>" /><br />
+								<img src="<?php echo $upload_path . '/' . self::upload_dir . '/' . date('Y/m/d/', $entry_date) . $entry_date; ?>/<?php echo $size['id']; ?>.jpg" /><br />
 								<p><?php echo date('n/j/Y \a\t g:i a', $entry_date); ?></p>
 								<div class="meta">
 									<?php foreach ($entry['metas'] as $meta) : ?>
